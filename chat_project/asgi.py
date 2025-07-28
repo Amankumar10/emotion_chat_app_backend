db16dfe03b7a74a820d897c1a0cbab88c2966cfd
Amankumar10 <amankumar02740@gmail.com>
2025-07-28 22:50:51 +0530

    websocket setup successfully done in backend now ready go in React WebSocket UI  (Step 6)

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -23,7 +23,7 @@ import django
 from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotion_chat_app_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
